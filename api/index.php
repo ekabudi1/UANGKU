996cbf66b9a80c3a9d7bc7b6bb142ddc49d34dd7
ekabudi1 <ekabudip96@gmail.com>
2026-08-27 16:36:29 +0800

    Fix serverless storage path and logs for Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,23 @@
 <?php
 
-    require_once __DIR__ . '/../public/index.php';
+// Paksa direktori kerja dan storage Laravel ke folder temporary serverless
+$_ENV['APP_STORAGE'] = '/tmp';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+$_ENV['LOG_CHANNEL'] = 'stderr';
+
+// Buat struktur folder sementara di /tmp jika belum ada
+$dirs = [
+    '/tmp/views',
+    '/tmp/framework/views',
+    '/tmp/framework/sessions',
+    '/tmp/framework/cache',
+];
+
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
+require __DIR__ . '/../public/index.php';
